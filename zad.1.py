f10d9273dbee65019cdd5c1fbc9d918eaf669a45
Marta Broniszewska <marta.broniszewska1@edu.uekat.pl>
2025-12-03 11:51:16 +0100

    Uzupełnienie kodu w zad.1

---
+++ zad.1.py
@@ -0,0 +1,7 @@
+def wyświetl_imiona(lista_imion):
+    for imie in lista_imion:
+        print(imie)
+
+lista_imion = ["Adam", "Marek", "Tomasz", "Bartosz", "Grzegorz"]
+wyświetl_imiona(lista_imion)
+
